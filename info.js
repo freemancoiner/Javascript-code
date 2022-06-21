14447482cbd3ce82788aa30a8a0336effaa241f4
Kachi-web <freeman.ng01@gmail.com>
2022-06-21 03:39:05 -0700

    initial commit

---
+++ info.js
@@ -0,0 +1,12 @@
+// console.log ("My name, height, and country")
+// console.table ({Name: "Gabriel", Height: "5'1'", Country: "Nigeria" })
+
+let Name = "Gabriel"
+let aHeight = "5.1 ft"
+let Country = "Nigeria"
+
+console.log("Name : "+Name)
+console.log("Height : "+aHeight)
+console.log("Country : "+Country)
+
+
